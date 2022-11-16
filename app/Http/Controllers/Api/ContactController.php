08b3ec8808a3d879216b4ade576ff83c513f1562
swiftank <ankk199217@gmail.com>
2022-11-16 13:23:40 +0000

    add pagination in contacts

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -15,7 +15,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        $contacts = Contact::all();
+        $contacts = Contact::paginate(10);
         return response()->json([
          'status' => true,
          'contacts' => $contacts,
